39bc542479fac7025d4e474026c45cbdd21a5dc9
d77commerce <d77commerce@hotmail.com>
2022-12-22 11:07:13 +0000

    update

---
+++ CourierService.Core/Models/Tracking/TrackingModel.cs
@@ -0,0 +1,27 @@
+﻿using CourierService.Infrastructure.Data.Models.Tracking;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierService.Core.Models.Tracking
+{
+    public class TrackingModel
+    {
+       
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string TrackingNo { get; set; } = null!;
+        [Required]
+        public string DeliveryPostcode { get; set; } = null!;
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public string? UserId { get; set; }
+      
+        [Required]
+        public string Info { get; set; } = "Thanks ! ! !";
+    }
+}
---
+++ CourierService/Controllers/TrackingController.cs
@@ -0,0 +1,18 @@
+﻿using CourierService.Core.Models.Tracking;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourierService.Controllers
+{
+    public class TrackingController : Controller
+    {
+     
+        [HttpGet]
+        [AllowAnonymous]
+       public IActionResult Index()
+        {
+          //  var model = new TrackingModel();
+            return View();
+        }
+    }
+}
